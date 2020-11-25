c627571ab2d7d0f7c8b20dbbf03e1ec88caf6fba
shimajiro4892 <endo1520123@gmail.com>
2020-11-26 01:06:05 +0900

    random number

--- java_dev/src/App.java
+++ java_dev/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //乱数発生
+        int randam = new java.util.Random() .nextInt(80);
+        //出力
+        System.out.println("今度のラッキーナンバーは"+ randam + "です");
     }
 }
