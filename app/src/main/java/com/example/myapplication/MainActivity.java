7dd91ce5c3a0a2b3610f0f82a438bcc2a3fc95d1
Hiro <kyoko4892@yahoo.co.jp>
2021-01-13 23:23:09 +0900

    テストアプリ作成開始

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //ボタンの実装
+        Button ohudabutton = findViewById(R.id.ohuda_button);
+        ohudabutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "家内安全", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
