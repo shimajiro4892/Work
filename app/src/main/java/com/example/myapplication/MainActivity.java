66cc3ce58c512aca6d36b2632104d039b73d0927
Hiro <kyoko4892@yahoo.co.jp>
2021-01-13 23:40:00 +0900

    ランダム選択肢追加

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,7 +20,29 @@ public class MainActivity extends AppCompatActivity {
         ohudabutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "家内安全", Toast.LENGTH_SHORT).show();
+                String contents = "";
+                Random random = new Random();
+                switch (random.nextInt(5)){
+                    case 0:
+                        contents = "家内安全";
+                        break;
+                    case 1:
+                        contents = "厄除け";
+                        break;
+                    case 2:
+                        contents = "健康祈願";
+                        break;
+                    case 3:
+                        contents = "交通安全";
+                        break;
+                    case 4:
+                        contents = "書くことないわ";
+                        break;
+                    case 5:
+                        contents = "眠い";
+                        break;
+                }
+                Toast.makeText(MainActivity.this, contents, Toast.LENGTH_SHORT).show();
             }
         });
     }
